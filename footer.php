8c4a8f70ef1ba40b912e5e408bd8c81f9e269380
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2024-09-30 17:49:03 +1000

    formatting updates

--- footer.php
+++ footer.php
@@ -29,6 +29,30 @@ $container = get_theme_mod( 'understrap_container_type' );
 							<!--Social-->
 							<ul class="cs-social">
 								<li class="cs-social-li">
+										<button type="button" class="subscribe-box btn" data-bs-toggle="modal" data-bs-target="#newsletterModal">
+											Subscribe
+										</button>
+										<!-- Modal -->
+										<div class="modal fade" id="newsletterModal" tabindex="-1" aria-labelledby="newsletterModalLabel" aria-hidden="true">
+										<div class="modal-dialog modal-dialog-centered">
+											<div class="modal-content">
+											<div class="modal-header">
+												<h1 class="modal-title fs-4" id="newsletterModalLabel">Subscribe to our Newsletter</h1>
+												<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+											</div>
+											<div class="modal-body">
+											<?php echo do_shortcode( '[wpforms id="2038" title="false"]' ); ?>
+											</div>
+											<!-- <div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+												<button type="button" class="btn btn-primary">Save changes</button>
+											</div> -->
+											</div>
+										</div>
+										</div>
+									
+								</li>
+								<li class="cs-social-li">
 									<a href="mailto:georgie@gbartadvisory.com.au" class="cs-social-link" aria-label="" target="_blank" rel="noopener">
 									<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-envelope-heart-fill" viewBox="0 0 16 16">
 										<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555l-4.2 2.568-.051-.105c-.666-1.3-2.363-1.917-3.699-1.25-1.336-.667-3.033-.05-3.699 1.25l-.05.105zM11.584 8.91l-.073.139L16 11.8V4.697l-4.003 2.447c.027.562-.107 1.163-.413 1.767Zm-4.135 3.05c-1.048-.693-1.84-1.39-2.398-2.082L.19 12.856A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144L10.95 9.878c-.559.692-1.35 1.389-2.398 2.081L8 12.324l-.551-.365ZM4.416 8.91c-.306-.603-.44-1.204-.413-1.766L0 4.697v7.104l4.49-2.752z"/>
